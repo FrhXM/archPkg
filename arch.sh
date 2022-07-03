54b38bc2816907ea542b0ed8e331ef936bc79d40
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-07-03 18:54:49 +0100

    Add files via upload

---
+++ arch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash 
+                            # == MY BASE PKG INSTALLER [ Arch ] == #
+sudo pacman -Syy xorg-server xorg-xinit xorg-xmessage xwallpaper acpilight xmonad xmonad-contrib xmobar kitty \
+    firefox chromium \
+    dmenu rofi \
+    fzf fd exa bat xclip trash-cli man-db imagemagick \
+    scrot slock dunst libnotify \
+    neovim sxiv mpv cmus \
+    thunar gvfs gvfs-mtp \
+    unzip unrar dosfstools ntfs-3g \
+    unclutter php picom lxappearance redshift \
+    arc-gtk-theme papirus-icon-theme \
+    ttf-dejavu ttf-font-awesome ttf-jetbrains-mono \
+    pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulsemixer pamixer \
+    bluez bluez-utils blueman \
+    xdg-user-dirs neofetch htop 
