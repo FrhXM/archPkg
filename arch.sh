f0d2d5523e4a0c6c4e31bcb63c920afdb05bb313
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-08-17 21:08:12 +0100

    Add files via upload

--- arch.sh
+++ arch.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syy --needed xorg-server xorg-xinit xorg-xmessage xwallpaper acpilight xmonad xmonad-contrib xmobar kitty \
     firefox qutebrowser \
     dmenu rofi \
-    fzf fd exa bat xclip trash-cli man-db imagemagick xdotool \
+    fzf fd exa bat xclip trash-cli man-db imagemagick xdotool acpi \
     scrot slock dunst libnotify \
     neovim sxiv mpv cmus \
     thunar thunar-volman thunar-archive-plugin \
