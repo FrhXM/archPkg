7d1286deb4bf2829085721c01ff57c8eeecf23f9
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-11-25 14:04:07 +0100

    Add files via upload

--- arch.sh
+++ arch.sh
@@ -5,13 +5,13 @@ sudo pacman -Syy --needed xorg-server xorg-apps xorg-xinit xorg-xmessage xwallpa
     dmenu rofi \
     fzf fd exa bat xclip trash-cli man-db imagemagick xdotool acpi \
     scrot slock dunst libnotify \
-    neovim sxiv mpv \
+    neovim neovide sxiv mpv \
     nemo udisks2 udiskie \
     gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-nfs gvfs-gphoto2 \
     unzip unrar dosfstools ntfs-3g \
     unclutter picom lxappearance-gtk3 redshift \
     arc-gtk-theme papirus-icon-theme \
-    python nodejs npm vscode \
+    nodejs npm \
     ttf-dejavu ttf-font-awesome ttf-jetbrains-mono \
     pipewire pipewire-pulse wireplumber pipewire-alsa pipewire-jack pipewire-zeroconf pamixer pulsemixer \
     bluez bluez-utils blueman network-manager-applet stalonetray \
